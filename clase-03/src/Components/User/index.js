12dfeb822aea4add5c758c5ff050fed4615b95ca
Brayan LP <brayansystemlp@gmail.com>
2019-12-05 18:02:50 -0500

    feat: demostración base de componente user

--- clase-03/src/Components/User/index.js
+++ clase-03/src/Components/User/index.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
+const API = "https://api.github.com/users/brayanlp";
 class User extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      user: {},
+      isLoading: true
+    };
   }
+
+  componentDidMount() {
+    this.user();
+  }
+  user = async () => {
+    const user = await axios.get(API).then(result => result.data);
+    this.setState({ user, isLoading: false });
+  };
   render() {
     const {
-      foto = 'https://avatars1.githubusercontent.com/u/14982190?s=460&v=4',
-      title = 'Brayan Laureano Paucar'
+      user: { avatar_url: foto, name: title },
+      isLoading
     } = this.state;
     return (
       <>
-        <div className="card">
-          <picture className="card-picture">
-            <img src={foto} alt="Foto"></img>
-          </picture>
-          <div className="card-title">{title}</div>
-        </div>
+        {isLoading ? (
+          <p>Cargando ....</p>
+        ) : (
+          <div className="card">
+            <picture className="card-picture">
+              <img src={foto} alt="Foto"></img>
+            </picture>
+            <div className="card-title">{title}</div>
+          </div>
+        )}
       </>
     );
   }
