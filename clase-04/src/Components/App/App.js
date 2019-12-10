3ad0c4b0fe2dc55a65ade01bdecd51e493f636ad
Brayan LP <brayansystemlp@gmail.com>
2019-12-10 17:44:50 -0500

    curso-04 router activo

--- clase-04/src/Components/App/App.js
+++ clase-04/src/Components/App/App.js
@@ -4,6 +4,7 @@ import { Router, Link } from '@reach/router';
 import Page1 from '../Page1';
 import Page2 from '../Page2';
 import Page3 from '../Page3';
+import Page3Detail from '../Page3/detail';
 
 function App() {
   return (
@@ -19,12 +20,16 @@ function App() {
           <li>
             <Link to="page3">Servicios</Link>
           </li>
+          <li>
+            <Link to="page3/clase-04">Detalles</Link>
+          </li>
         </ul>
       </header>
       <Router>
         <Page1 path="/" />
         <Page2 path="/page2" />
-        <Page3 path="/page3" />
+        <Page3 path="/page3"></Page3>
+        <Page3Detail path="/page3/:detalleId"></Page3Detail>
       </Router>
     </div>
   );
---
+++ clase-04/src/Components/Page3/detail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Page3Detail = ({ detalleId }) => {
+  return (
+    <>
+      <h1>Detail Page 3: {detalleId} </h1>
+    </>
+  );
+};
+
+export default Page3Detail;
