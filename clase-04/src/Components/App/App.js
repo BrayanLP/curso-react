d3a8378e30948d89ae077902f1bfdaa7a820d126
Brayan LP <brayansystemlp@gmail.com>
2019-12-10 16:30:35 -0500

    proyecto base clase 04

---
+++ clase-04/src/Components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <ul>
+          <li>
+            <a href="">Inicio</a>
+          </li>
+          <li>
+            <a href="">Nosotros</a>
+          </li>
+          <li>
+            <a href="">Servicios</a>
+          </li>
+          <li>
+            <a href="">Contacto</a>
+          </li>
+        </ul>
+      </header>
+      <h1>APP</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ clase-04/src/Components/Page1/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <h1> Página 1</h1>
+      </>
+    );
+  }
+}
