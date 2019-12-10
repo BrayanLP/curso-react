6ce9a70af6d33caa083b5c116606751ba132e2c4
Brayan LP <brayansystemlp@gmail.com>
2019-12-10 17:26:08 -0500

    curso 04 reach router

--- clase-04/src/Components/App/App.js
+++ clase-04/src/Components/App/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import { Router, Link } from '@reach/router';
+import Page1 from '../Page1';
+import Page2 from '../Page2';
+import Page3 from '../Page3';
 
 function App() {
   return (
@@ -7,20 +11,21 @@ function App() {
       <header>
         <ul>
           <li>
-            <a href="">Inicio</a>
+            <Link to="/">Inicio</Link>
           </li>
           <li>
-            <a href="">Nosotros</a>
+            <Link to="page2">Nosotros</Link>
           </li>
           <li>
-            <a href="">Servicios</a>
-          </li>
-          <li>
-            <a href="">Contacto</a>
+            <Link to="page3">Servicios</Link>
           </li>
         </ul>
       </header>
-      <h1>APP</h1>
+      <Router>
+        <Page1 path="/" />
+        <Page2 path="/page2" />
+        <Page3 path="/page3" />
+      </Router>
     </div>
   );
 }
